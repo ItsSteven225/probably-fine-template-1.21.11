dffe2800bc823b578f7b1de8473276a66f697d91
ItsSteven225 <sup3rsteven1@gmail.com@ejemplo.com>
2026-03-13 23:19:01 -0500

    asds

--- src/main/java/net/itssteven/probablyfine/ProbablyFine.java
+++ src/main/java/net/itssteven/probablyfine/ProbablyFine.java
@@ -2,22 +2,17 @@ package net.itssteven.probablyfine;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.itssteven.probablyfine.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProbablyFine implements ModInitializer {
 	public static final String MOD_ID = "probably-fine";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
--- src/main/java/net/itssteven/probablyfine/item/ModItems.java
+++ src/main/java/net/itssteven/probablyfine/item/ModItems.java
@@ -1,36 +1,19 @@
 package net.itssteven.probablyfine.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.itssteven.probablyfine.ProbablyFine;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-
-import java.util.function.Function;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item VILLAGER_FETUS = registerItem("villager_fetus", new Item(new Item.Settings()));
 
-    // TU ITEM
-    public static final Item VILLAGER_FETUS = register(
-            "villager_fetus",
-            Item::new,
-            new Item.Settings()
-    );
-
-    public static <T extends Item> T register(String name, Function<Item.Settings, T> itemFactory, FabricItem.Settings settings) {
-
-        Resource<Item> itemKey = ResourceKey.create(
-                Registries.ITEM,
-                new ResourceLocation(ProbablyFine.MOD_ID, name)
-        );
-
-        T item = itemFactory.apply(settings);
-
-        Registry.register(BuiltInRegistries.ITEM, itemKey.location(), item);
-
-        return item;
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ProbablyFine.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
+        ProbablyFine.LOGGER.info("Registering mod items for {}", ProbablyFine.MOD_ID);
     }
 }
