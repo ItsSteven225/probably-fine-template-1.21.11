7ae91ab8e809b77e20aa2d2aacab3c6517f8b13b
ItsSteven225 <sup3rsteven1@gmail.com>
2026-03-13 23:13:53 -0500

    Merge pull request #1
    * Fix item registration and wire datagen model provider

--- src/client/java/net/itssteven/probablyfine/datagen/ProbablyFineDataGenerator.java
+++ src/client/java/net/itssteven/probablyfine/datagen/ProbablyFineDataGenerator.java
@@ -7,7 +7,6 @@ public class ProbablyFineDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-
-        
+        pack.addProvider(ModModelProvider::new);
     }
 }
