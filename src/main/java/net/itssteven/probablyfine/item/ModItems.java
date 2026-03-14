f769b2fd4192dbc04742c70df286a3a2ff5e01c7
ItsSteven225 <sup3rsteven1@gmail.com@ejemplo.com>
2026-03-14 16:46:02 -0500

    gnshkj

--- src/main/java/net/itssteven/probablyfine/item/ModItems.java
+++ src/main/java/net/itssteven/probablyfine/item/ModItems.java
@@ -4,16 +4,24 @@ import net.itssteven.probablyfine.ProbablyFine;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static final Item VILLAGER_FETUS = registerItem("villager_fetus", new Item(new Item.Settings()));
+    public static final Item VILLAGER_FETUS = registerItem("villager_fetus", Item::new);
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(ProbablyFine.MOD_ID, name), item);
+    private static Item registerItem(String name, Function<Item.Settings, Item> itemFactory) {
+        Identifier id = Identifier.of(ProbablyFine.MOD_ID, name);
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = new Item.Settings().registryKey(itemKey);
+
+        Item item = itemFactory.apply(settings);
+            return Registry.register(Registries.ITEM, itemKey, item);
     }
-
-    public static void registerModItems() {
+        public static void registerModItems() {
         ProbablyFine.LOGGER.info("Registering mod items for {}", ProbablyFine.MOD_ID);
     }
 }
